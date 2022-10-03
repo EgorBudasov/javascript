3b8ffe313412104813c71f6bb607b6e59821c2b0
Egor <egor.budasow@gmail.com>
2022-10-03 16:43:29 +0300

    first commit

---
+++ javaScript/script.js
@@ -0,0 +1,77 @@
+ "use strict"; 
+// let number = 5;
+// const leftBorderWidth  = 2;
+// console.log(leftBorderWidth);
+// number = 40;
+// console.log(number);
+// console.log(number + leftBorderWidth);
+
+
+// const obj = {
+//      name : 'Peter',
+//      age : 32,
+//      isMarried : true,
+//      isChildren : false
+// };
+// console.log(obj.isMarried,obj.age,obj.name );
+
+
+// let storeName = 'rozetka';
+// const storeDescription = {
+//     budget : 10000,
+//     employees : ['peter','ivan', 'ania'],
+//     products : {
+//         'apple' : 30,
+//         'pineapple' : 40,
+        
+//     },
+
+//      open : true
+// };
+// storeDescription.dayOff = 'sunday';
+// console.log(storeName);
+
+// alert('Hellow');
+// const result = confirm('R U Here?');
+// console.log (result);
+ 
+// const answer = prompt('How old are you?','' );
+// console.log (typeof(answer));
+
+// const answers = [];
+// answers[0]= prompt('what is your name?','');
+// let users = answers[0];
+// answers[1] = prompt(`what is your lastname,${users}?`);
+// answers[2] = confirm('Тебе есть 18?');
+// console.log(answers);
+
+
+
+
+
+// let incr = 13,
+//     decr = 10;
+// incr++;//инкримент (увеличение на 1)постфиксный вид записи
+// --decr;//декримент(уменьшение на 1) префиксный вид записи
+// console.log(incr);
+// console.log(decr);
+
+
+// console.log(7%2);
+// console.log('Ghbdtn' == 'Ghbdtn'); //Оператор сравнения
+
+
+// let box = false,
+//     email = false;
+// console.log(box || email);//оперратор или
+
+
+// let a = false;
+// console.log(!a);//оператор отрицания
+
+
+// let user = 'Egor';
+// const hellow = confirm(`Hellow,${user} ${'Are u okay'}`);
+// console.log(hellow);
+
+
